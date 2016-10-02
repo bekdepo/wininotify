7bb6641c9994b70cde11bdebc820e9cc5399bb7d
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2016-10-02 22:17:48 +0530

    Targated and tested with Windows 7

--- wininotifyDriver/FsEntry.c
+++ wininotifyDriver/FsEntry.c
@@ -9,10 +9,8 @@ DRIVER_INITIALIZE DriverEntry;
 NTSTATUS DriverEntry( PDRIVER_OBJECT pDriverObject,
 					  PUNICODE_STRING pRegistryPath ) 
 {
-	NTSTATUS status = STATUS_SUCCESS;
-	
 	UNREFERENCED_PARAMETER(pDriverObject);
 	UNREFERENCED_PARAMETER(pRegistryPath);
 
-	return status;
+	return STATUS_SUCCESS;
 }
