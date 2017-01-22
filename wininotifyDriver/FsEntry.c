7ae82e6f3aded343b158f803c3db6dff826ce3ec
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2017-01-22 13:58:11 +0530

    Added initial filter registartion data
    This is dummy structure, will fill the other members later one at a time

--- wininotifyDriver/FsEntry.c
+++ wininotifyDriver/FsEntry.c
@@ -10,6 +10,29 @@ NTSTATUS winnotifyInstanceSetup(
 	_In_ FLT_FILESYSTEM_TYPE VolumeFsType
 );
 
+CONST FLT_REGISTRATION FilterRegistration = {
+	sizeof(FLT_REGISTRATION),
+	FLT_REGISTRATION_VERSION,
+	0,
+
+	NULL,						// Context registartion
+	NULL,						// Operation Callbacks
+
+	NULL,						// Unload routine
+
+	winnotifyInstanceSetup,		// Instance Setup
+	NULL,						// Query tear down
+	NULL,						// Teardown start
+	NULL,						// Teardown completed
+
+	NULL,						// Generate file name
+	NULL,						// Name normalizer
+	NULL,						// Normalizer context clean up
+	NULL,						// Transaction notification callback
+	NULL,						// Extended name normalizer callback
+	NULL						// Section notification callback
+};
+
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text(INIT, DriverEntry)
 #pragma alloc_text(PAGE, winnotifyInstanceSetup)
